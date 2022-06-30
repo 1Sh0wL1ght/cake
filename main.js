a5786c2d0cb970732119a86713fe4de977df86eb
1Sh0wL1ght <108289541+1Sh0wL1ght@users.noreply.github.com>
2022-06-30 11:05:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,24 +7,28 @@ const { usernames } = require('./usernames.json')
 //SETTINGS
 let inValidCapePath = __dirname + '/capes/invalidcape.png'
 
-
+console.log("--------------------------------------------------------------------------------------------------------------------------------------")
 console.log("██████╗░███████╗██╗░░░░░███████╗████████╗███████╗░█████╗░██████╗░████████╗██╗███████╗██╗███╗░░██╗███████╗░░░███╗░░██╗███████╗████████╗")
 console.log("██╔══██╗██╔════╝██║░░░░░██╔════╝╚══██╔══╝██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██║██╔════╝██║████╗░██║██╔════╝░░░████╗░██║██╔════╝╚══██╔══╝")
 console.log("██║░░██║█████╗░░██║░░░░░█████╗░░░░░██║░░░█████╗░░██║░░██║██████╔╝░░░██║░░░██║█████╗░░██║██╔██╗██║█████╗░░░░░██╔██╗██║█████╗░░░░░██║░░░")
 console.log("██║░░██║██╔══╝░░██║░░░░░██╔══╝░░░░░██║░░░██╔══╝░░██║░░██║██╔═══╝░░░░██║░░░██║██╔══╝░░██║██║╚████║██╔══╝░░░░░██║╚████║██╔══╝░░░░░██║░░░")
 console.log("██████╔╝███████╗███████╗███████╗░░░██║░░░███████╗╚█████╔╝██║░░░░░░░░██║░░░██║██║░░░░░██║██║░╚███║███████╗██╗██║░╚███║███████╗░░░██║░░░")
 console.log("╚═════╝░╚══════╝╚══════╝╚══════╝░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚══╝╚══════╝╚═╝╚═╝░░╚══╝╚══════╝░░░╚═╝░░░")
-console.log()
+console.log("--------------------------------------------------------------------------------------------------------------------------------------")
 console.log("Cape manipulation for MCP Optifine Sources Only!")
 console.log("Made By 1Sh0wL1ght")
 console.log()
 
 app.get('/', (req, res) => {
+  console.log("New request from: " + req.ip);
+  console.log("Sent \"/\"");
   res.send('<h1>local cape system</h1>\n<h2>provided by 1sh0wl1ght</h2>')
+  
 })
 app.get('/reset', (req, res) => {
   res.send('done, reload this page to again reset capes. to reload capes => Options>Skin customization>Optifine Cape>Reload Cape')
-  console.log("sending changed capes")
+  console.log("Recived a reload action!")
+  console.log("Sending changed capes")
   capesys()
 })
 console.log("loading capes for users")
@@ -32,17 +36,19 @@ function capesys() {
     for (const username of usernames) {
       let capepath = __dirname + '/capes/' + username + '.png'
       if(fs.existsSync(__dirname + '/capes/' + username + '.png')) {
-        console.log(`sent ${username}'s cape!`)
+        console.log(`Loaded ${username}'s cape!`)
       } else {
         capepath = inValidCapePath
-        console.log(`invalid cape for ${username}!`)
+        console.log(`Invalid cape for ${username}!`)
       }
       app.get(`/${username}`, (req, res) => {
         if(fs.existsSync(__dirname + '/capes/' + username + '.png')) {
-          console.log(`sent ${username}'s cape, as a response`)
+          console.log("New Cape request from: " + req.ip);
+          console.log(`Sent ${username}'s cape, as a response`)
           capepath = __dirname + '/capes/' + username + '.png'
         } else {
           capepath = inValidCapePath
+          console.log("New Cape request from: " + req.ip);
           console.log(`invalid cape for ${username}, sent Invaild cape instead!`)
         }
         res.sendFile(capepath)
